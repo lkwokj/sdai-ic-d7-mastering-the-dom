79d7ac8cc9c006a492b071cb20047ad9a669d995
Lily Kwok <lkwokj@gmail.com>
2024-11-15 04:30:31 +0000

    updated to remove boxes

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Write your JavaScipt code below this line. 
+const mainTitle = document.getElementById("main-title")
+mainTitle.textContent = "Welcome to Our Cat Adoption Center"
+
+const description = document.getElementById("description")
+description.textContent = "Find your purrfect Companion!"
+
+const cat1Button = document.getElementById("cat-1-button")
+cat1Button.addEventListener("click", () => {
+    const cat = document.getElementById("cat-1")
+    alert("Congrats on adopting you new furry friend!")
+    cat.remove()
+})
+
+const cat2Button = document.getElementById("cat-2-button")
+cat2Button.addEventListener("click", () => {
+    const cat = document.getElementById("cat-2")
+    alert("Congrats on adopting you new furry friend!")
+    cat.remove()
+})
+
+const cat3Button = document.getElementById("cat-3-button")
+cat3Button.addEventListener("click", () => {
+    const cat = document.getElementById("cat-3")
+    alert("Congrats on adopting you new furry friend!")
+    cat.remove()
+})
